fix constraint violation formula

ncopt/sqpgs/main.py
@@ -211,8 +211,10 @@ class SQPGS:
                      
             # Logging, start after first iteration
             if iter_k % self.log_every == 1:
-                _viol =  np.max(np.hstack((gI_k, np.abs(gE_k))))
-                self.logger.info(f"Iter {iter_k}, objective {f_k:.3E}, constraint violation {_viol:.3E}, accuracy {E_k:.3E}, avg runtime/iter {(1e3)*np.mean(self.timings['total']):.3E} ms.")
+                violI_k =  np.maximum(gI_k, 0)
+                violE_k = np.abs(gE_k)
+                viol_k = np.max(np.hstack((violI_k, violE_k)))
+                self.logger.info(f"Iter {iter_k}, objective {f_k:.3E}, constraint violation {viol_k:.3E}, accuracy {E_k:.3E}, avg runtime/iter {(1e3)*np.mean(self.timings['total']):.3E} ms.")
 
             new_E_k = stop_criterion(self.gI, self.gE, g_k, self.SP, gI_k, gE_k, B_gI, B_gE, self.nI_, self.nE_, pI, pE)
             E_k = min(E_k, new_E_k)