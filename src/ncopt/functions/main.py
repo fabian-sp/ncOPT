db8ffbfa6750ffe539a7ee6b856f97c338d724d1
fabian-sp <fabian.schaipp@hotmail.de>
2024-03-13 19:54:08 +0100

    add differentiability flag

--- src/ncopt/functions/main.py
+++ src/ncopt/functions/main.py
@@ -1,15 +1,23 @@
+from typing import Callable, Optional
+
 import torch
 
 
 class ObjectiveOrConstraint(torch.nn.Module):
-    def __init__(self, model: torch.nn.Module, name: str = None, prepare_inputs=None):
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        name: Optional[str] = None,
+        prepare_inputs: Optional[Callable] = None,
+        is_differentiable: bool = False,
+    ):
         super().__init__()
 
-        self.prepare_inputs = prepare_inputs
+        # TODO: Load from checkpoint if this is a string
         self.model = model
-        # Optional: load from checkpoint if this is a string
-
         self.name = name
+        self.prepare_inputs = prepare_inputs
+        self.is_differentiable = is_differentiable
 
         # Go into eval mode
         self.model.eval()
